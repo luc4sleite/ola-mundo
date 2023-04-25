80b96a3761ad074ae38ee752457086a06571d2d0
luc4sleite <lucahleite@gmail.com>
2023-04-25 16:48:51 -0300

    feat: cards da pag inicial e modificação das rotas

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,11 +1,17 @@
-import Banner from "componentes/Banner";
+import Post from "componentes/Post";
+import styles from "./Inicio.module.css";
+
+import posts from 'json/posts.json'
+
 
 export default function Inicio() {
   return (
-    <main>
-      <Banner />
-
-      <h1>Olá mundo!</h1>
-    </main>
+      <ul className={styles.posts}>
+        {posts.map((post) => (
+          <li key={post.id}>
+              <Post post={post}/>
+          </li>
+        ))}
+      </ul>
   )
 }
